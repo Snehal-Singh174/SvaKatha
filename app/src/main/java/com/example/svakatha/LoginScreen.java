19e5ccabeb28d9c0808769e8c5e5f9e786d4e14e
Naiket08 <60822686+Naiket08@users.noreply.github.com>
2020-02-10 18:02:44 +0530

    Update LoginScreen.java

--- app/src/main/java/com/example/svakatha/LoginScreen.java
+++ app/src/main/java/com/example/svakatha/LoginScreen.java
@@ -35,17 +35,6 @@ public class LoginScreen extends AppCompatActivity implements View.OnClickListen
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
 
-//        View decorView = getWindow().getDecorView();
-//        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.KITKAT){
-//            decorView.setSystemUiVisibility(
-//                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-//                            |View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-//                            |View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-//                            |View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-//                            |View.SYSTEM_UI_FLAG_FULLSCREEN
-//                            |View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-//        }
-
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
 
