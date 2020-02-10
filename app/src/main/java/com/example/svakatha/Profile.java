04e2d39be41738a924a1362788812e975191d4ad
Naiket08 <60822686+Naiket08@users.noreply.github.com>
2020-02-10 18:09:49 +0530

    Update Profile.java

--- app/src/main/java/com/example/svakatha/Profile.java
+++ app/src/main/java/com/example/svakatha/Profile.java
@@ -38,18 +38,6 @@ public class Profile extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
 
-//        View decorView = getWindow().getDecorView();
-//        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.KITKAT){
-//            decorView.setSystemUiVisibility(
-//                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-//                        |View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-//                        |View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-//                        |View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-//                        |View.SYSTEM_UI_FLAG_FULLSCREEN
-//                        |View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-//        }
-
-
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
