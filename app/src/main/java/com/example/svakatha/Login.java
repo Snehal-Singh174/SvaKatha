104a1383f927f0683ae81723fd87747735025929
Siddhi Nagarkar <siddhinagarkar1@gmail.com>
2020-02-29 11:07:57 +0530

    Progress bar

--- app/src/main/java/com/example/svakatha/Login.java
+++ app/src/main/java/com/example/svakatha/Login.java
@@ -64,6 +64,7 @@ public class Login extends AppCompatActivity {
                         if(task.isSuccessful()){
                             Toast.makeText(Login.this,"Login Successful",Toast.LENGTH_SHORT).show();
                             startActivity(new Intent(getApplicationContext(),Profile.class));
+
                         }else{
                             Toast.makeText(Login.this,"Error! " + task.getException().getMessage(),Toast.LENGTH_SHORT).show();
                         }
